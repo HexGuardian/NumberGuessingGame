fa2b24b8a0b7410b52ef78fee9a8ba88a298ff52
HexGuardian <ash.personal12390@gmail.com>
2023-09-23 21:33:39 +0530

    final

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const playerGuess = document.getElementById("guess");
 const checkButton = document.getElementById("checkGuess");
 const message = document.getElementById("message");
+const guessTracker = document.getElementById("trackGuess");
+const restartButton = document.getElementById("restart"); // Use restartButton here
 
-// Generate the random number when the game starts
-const randomNumber = generateNumber();
+let numberOfGuesses = 0;
+let randomNumber = generateNumber();
 
 // Callback function to generate a random number
 function generateNumber() {
@@ -17,12 +19,37 @@ function checkGuess() {
   if (isNaN(guess) || guess < 1 || guess > 100) {
     message.textContent = "Please enter a valid number between 1 and 100.";
   } else if (guess === randomNumber) {
-    message.textContent = "Congratulations! You guessed the correct number!";
+    numberOfGuesses++;
+    message.textContent = `Congratulations! You guessed the correct number in ${numberOfGuesses} guesses`;
+    disableInput();
+    restartButton.style.display = "block"; // Display the Restart button
   } else if (guess > randomNumber) {
+    numberOfGuesses++;
     message.textContent = "Too high, try again.";
   } else {
+    numberOfGuesses++;
     message.textContent = "Too low, try again.";
   }
+  guessTracker.textContent = `Number of guesses: ${numberOfGuesses}`;
+}
+
+// Function to disable input and check button
+function disableInput() {
+  playerGuess.disabled = true;
+  checkButton.disabled = true;
+}
+
+// Function to reset the game
+function restartGame() {
+  numberOfGuesses = 0;
+  randomNumber = generateNumber(); // Regenerate the random number
+  playerGuess.value = "";
+  message.textContent = "";
+  guessTracker.textContent = "Number of guesses: 0";
+  playerGuess.disabled = false;
+  checkButton.disabled = false;
+  //restartButton.style.display = "none"; // Hide the Restart button
 }
 
 checkButton.addEventListener("click", checkGuess);
+restartButton.addEventListener("click", restartGame);
